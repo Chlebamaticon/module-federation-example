b126ec8bfcaf0fb3a57c544d7622a1c1fbb530a1
Jakub Chlebowicz <contact@jakubchlebowicz.com>
2025-03-27 14:08:18 +0100

    patch: updated events

--- apps/server/system-admin/src/main.ts
+++ apps/server/system-admin/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { eventNames } from '@inveniam/server-events';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,8 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    eventNames
   );
 }
 
